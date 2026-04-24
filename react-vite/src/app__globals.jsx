75bcd266636ad55824f1623692072f13c5a093f2
boomer <frank.earnhardt@outlook.com>
2026-04-24 12:00:58 -0400

    New file to store app global properties

---
+++ react-vite/src/app__globals.jsx
@@ -0,0 +1,6 @@
+//app_globals.jsx
+export const GLOBALS = {
+  bg_img:"/img/0.jpg"
+  //, bg_override_color: "#0f172a" // Tailwind's slate-900
+  //, bg_override_color: "blue" // Tailwind's slate-900
+};
