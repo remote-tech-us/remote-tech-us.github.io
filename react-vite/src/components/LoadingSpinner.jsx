049496764370c6931dfdebaecd5e6df9e79864ad
boomer <frank.earnhardt@outlook.com>
2026-05-15 11:05:39 -0400

    Use app_logo from data/app__globals.jsx

--- react-vite/src/components/LoadingSpinner.jsx
+++ react-vite/src/components/LoadingSpinner.jsx
@@ -1,12 +1,17 @@
+// src/components/LoadingSpinner.jsx
+
 import React from 'react';
+import { GLOBALS } from '../data/app__globals.jsx';
+
 // Use your actual logo path here
-import logo from '../assets/react.svg'; 
+//import logo from '../assets/react.svg'; 
+//import logo from '/remote-tech-us_v2.svg'; 
 
 const LoadingSpinner = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] space-y-4">
       <img 
-        src={logo} 
+        src={GLOBALS.app_logo} 
         className="w-16 h-16 animate-spin-slow" 
         alt="Loading..." 
       />
