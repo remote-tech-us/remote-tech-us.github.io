d51c8c3e2d8e13ed6c1e37ddc8c7adfd17f53593
boomer <frank.earnhardt@outlook.com>
2026-05-15 10:01:41 -0400

    npm install -D rollup-plugin-visualizer
    How to Identify "Heavy" Pages/Modules
      In the report that opened in your browser:
      The Treemap: You will see large rectangles. These represent your biggest dependencies.
      The "Vendor" Chunk: You’ll likely see a massive block for node_modules. Inside that, look for specific libraries like lucide-react, framer-motion, or lodash.
      The "Source" Chunk: Look for your /src folder. If a specific page (like About.jsx) has a significantly larger rectangle than others, it’s likely because of:
      Direct Imports: You might be importing a heavy library inside that file that isn't used elsewhere.
      Large Assets: If you see an image or a data file inside the JS bundle, it means it's being "inlined" instead of loaded as an external file.

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  //base: '/remote-tech.us/',//
+  plugins: [
+    react(), 
+    tailwindcss(),
+    visualizer({
+      open: true, // automatically opens the report in your browser after build
+      filename: "bundel-report.html",
+      gzipSize: true,
+    }),
+  ],
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // puts all libraries in a separate file
+          }
+        },
+      },
+    },
+  },
 })
